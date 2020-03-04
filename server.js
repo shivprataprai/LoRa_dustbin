da78a15e93d58e34ab9c3efd1a102b5ccfd2073e
shiv <shivprataprai11@gmail.com>
2020-03-05 01:40:36 +0530

    deploy

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app=express()
 
 app.use(bodyParser.json())
 app.post("/",async(req,res)=>{
+    console.log("got a request")
     console.log(JSON.stringify(req.body))
-    await DB.create(JSON.stringify(req.body))
+    await DB.create({note:JSON.stringify(req.body)})
     console.log("Ho gail")
     res.status(200).send("Hai gava")
 })
