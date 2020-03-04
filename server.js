6ca211f67b6c9d88e71cd20198c5d24853b78b22
shiv <shivprataprai11@gmail.com>
2020-03-05 01:08:11 +0530

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express=require('express')
 const mongoose=require('mongoose')
 const DB=require('./connection')
-const body_parser=require('body-parser')
+const bodyParser=require('body-parser')
 
-const app=exprress()
+const app=express()
 
 app.use(bodyParser.json())
-app.post("/",(req,res)=>{
+app.post("/",async(req,res)=>{
     console.log(JSON.stringify(req.body))
     await DB.create(JSON.stringify(req.body))
     console.log("Ho gail")
     res.status(200).send("Hai gava")
 })
 
-app.listen(process.env.PORT,()=>console.log("Shuru hoi gava"))
+app.listen(process.env.PORT||3000,()=>console.log("Shuru hoi gava"))
